022d309b57adc1b48e320864057e8f1b43ae991f
SoumyaD27 <soumyad233@gmail.com>
2022-12-14 15:31:15 +0530

    [Delete the first element in linkedlist]

---
+++ UC5_DeleteFirstElement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UC5_DeleteFirstElement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.pop(56);
+            list.Display();
+        }
+    }
+}
