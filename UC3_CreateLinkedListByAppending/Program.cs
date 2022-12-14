876a91d4d8b276f122ca7393520e7729f963c603
SoumyaD27 <soumyad233@gmail.com>
2022-12-14 13:04:14 +0530

    [Create LinkedList by appending 30 and 70 to 56]

---
+++ UC3_CreateLinkedListByAppending/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UC3_CreateLinkedListByAppending
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
