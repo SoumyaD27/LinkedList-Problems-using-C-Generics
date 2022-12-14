f0417d5aaf403a6f95594ed3009a8dba83e7d685
SoumyaD27 <soumyad233@gmail.com>
2022-12-14 13:29:36 +0530

    [Insert 30 between 56 and 70]

---
+++ UC4_InsertBetweenLinkedList/LinkedList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC4_InsertBetweenLinkedList
+{
+    internal class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public Node InsertAtParticularPosition(int position, int data)
+        {
+            if(position < 1)
+            {
+                Console.WriteLine("Invalid position");
+            }
+            if(position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while(position-- != 0)
+                {
+                    if(position==1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if(position!= 1) 
+                {
+                    Console.WriteLine("Position out of range");
+                }
+            }
+            return head;
+        }
+    }
+}
---
+++ UC4_InsertBetweenLinkedList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UC4_InsertBetweenLinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.InsertAtParticularPosition(2, 30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
