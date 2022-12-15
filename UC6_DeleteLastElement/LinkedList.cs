c6a119b4295069c4fc174f2f2161651a06b7c12f
SoumyaD27 <soumyad233@gmail.com>
2022-12-15 16:45:06 +0530

    [Delete the Last element]

---
+++ UC6_DeleteLastElement/LinkedList.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC6_DeleteLastElement
+{
+    public class LinkedList
+    {
+        public Node head;
+        public Node tail;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+                {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public Node popLast()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node NewNode = head;
+            while (NewNode.next.next!= null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+            return head;
+        }
+    }
+}
---
+++ UC6_DeleteLastElement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace UC6_DeleteLastElement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.popLast();
+            list.Display();
+        }
+    }
+}
